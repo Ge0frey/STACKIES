98a8052f123f707815284a79973d111efd87469e
Ge0frey <jeffkimzyjoe@gmail.com>
2025-02-13 00:43:10 +0300

    Declare an empty todo

--- SOLIDITY-REFRESH-TODO/todos/src/TODOS.sol
+++ SOLIDITY-REFRESH-TODO/todos/src/TODOS.sol
@@ -2,13 +2,17 @@
 pragma solidity ^0.8.20;
 
 contract Todos {
-	struct TodoItem {
-    	uint256 todoID;
-    	string todoTitle;
-    	string todoDescription;
-    	bool completed;
-    	address todoOwner;
-	}
+    struct TodoItem {
+        uint256 todoID;
+        string todoTitle;
+        string todoDescription;
+        bool completed;
+        address todoOwner;
+    }
+
+    // Use this as a reference when referring to an empty TODO
+    TodoItem public EMPTY_TODO =
+        TodoItem(0, "", "", false, 0x0000000000000000000000000000000000000000);
 
     mapping(uint256 => TodoItem) public todos;
 
